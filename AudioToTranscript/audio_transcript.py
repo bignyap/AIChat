086da2da7d4d0f730a73206b4d5edcdf8ed81997
Bignya <bignyap@wolfram.com>
2023-10-02 01:31:20 +0530

    Convert to snake_case

---
+++ AudioToTranscript/audio_transcript.py
@@ -0,0 +1,36 @@
+# os library
+import os
+
+# Whisper to detect the language
+import whisper
+
+
+def audio_to_transcript(file: str) -> str:
+    """
+    This is a docstring for audio_to_transcript.
+
+    Args:
+        file (str): Audio file
+
+    Returns:
+        str: Path to the transcript filr
+    """
+
+    # filename = os.path.basename(file)
+    filename = os.path.splitext(file)[0]
+    filename = ".".join([filename, "txt"])
+
+    # Load the base model and transcribe the audio
+    model = whisper.load_model("base")
+    result = model.transcribe(file)
+    transcribed_text = result["text"]
+
+    with open(filename, "w", encoding='utf-8') as file1:
+        file1.write(transcribed_text)
+
+    return transcribed_text
+
+
+if __name__ == "__main__":
+    AUDIO_FILEPATH = str(input("Audio filepath: "))
+    print(audio_to_transcript(AUDIO_FILEPATH))
--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 from fastapi import FastAPI, UploadFile
-from CompanyName import CompanyName
-from Summarizer import Summarizer
+from CompanyName import company_name
+from PDFSummarizer import pdf_summarizer
 
 app = FastAPI()
 
 @app.get("/getCompanyName")
-def company_name(product: str, howmany:int = 1):
-    return CompanyName.llm_company_name(product, howmany)
+def llm_name(product: str, howmany:int = 1) -> str:
+    """
+    This is a docstring for llm_name.
+
+    Args:
+        product (str): string
+
+    Returns:
+        str: Path to the audio file
+    """
+    return company_name.llm_company_name(product, howmany)
 
 
 @app.post("/summarize")
-async def llm_summarize(file: UploadFile):
-    filepath = await Summarizer.PDFReader(file)
-    res =  Summarizer.PDFSummarizer(filepath)
+async def llm_summarize(file: UploadFile) -> str:
+    """
+    This is a docstring for llm_summarize.
+
+    Args:
+        file (UploadFile): UplaodFile
+
+    Returns:
+        str: Path to the audio file
+    """
+    filepath = await pdf_summarizer.PDFReader(file)
+    res =  pdf_summarizer.PDFSummarizer(filepath)
     return res
     
 
