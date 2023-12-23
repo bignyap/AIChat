16b07a793fe2cc9c9b7f53d8f7d6f5821b78efc0
bignyap <bignyap@wolfram.com>
2023-12-23 22:59:18 +0530

    Adding authentction to all microservices

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -1,57 +1,75 @@
-''' Gatway doc string'''
+'''
+Module doc string
+'''
 
-from fastapi import FastAPI, Request, UploadFile
+import os
+import requests
 
-from auth_svc import access
+from fastapi import FastAPI, Request, UploadFile, HTTPException
+
 from company_name import company_name
 from pdf_summarizer import pdf_summarizer
-from audio_to_text import audio_to_text
+
+AUTH_SVC_ADDRESS = os.getenv("AUTH_SVC_ADDRESS")
+TIMEOUT_LIMIT = int(os.getenv("TIMEOUT_LIMIT"))
 
 app = FastAPI()
 
-@app.post("/login")
-async def login(
-    request: Request
-):
-    ''' doc string '''
+async def authenticate(request: Request) -> str:
+    '''
+    Authenticate the User
+    This function gets the Authorization header from the request, calls 
+    the authenticating service, and return the token if valid.
+    '''
 
-    token, err = access.login(request)
+    token = request.headers.get("Authorization")
+    if not token:
+        raise HTTPException(status_code=401, detail="Unauthorized: Token missing")
 
-    if not err:
-        return token
+    response = requests.post(
+        f"{AUTH_SVC_ADDRESS}/validate", 
+        headers={"Authorization": token},
+        timeout = TIMEOUT_LIMIT
+    )
     
-    return err
+    if response.status_code != 200:
+        raise HTTPException(status_code=401, detail="Unauthorized: Invalid token")
+    
+    return token
+
+@app.post("/summarize")
+async def llm_summarize(request: Request, file: UploadFile) -> str:
+    """
+    Endpoint to summarize a PDF file.
+    
+    """
+
+    try:
+        _ = await authenticate(request)
+
+        filepath = await pdf_summarizer.pdf_reader(file)
+        res = pdf_summarizer.pdf_summarizer(filepath)
+
+        return res
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error") from e
 
 
 @app.get("/getCompanyName")
-def llm_name(product: str, howmany:int = 1) -> str:
+async def llm_name(request: Request, product: str, howmany:int = 1) -> str:
     """
-    This is a docstring for llm_name.
-
-    Args:
-        product (str): string
-
-    Returns:
-        str: Path to the audio file
+    Endpoint to get company names based on product query.
+    The number of results is set to 1 by default and can be changed by setting the 'howmany' parameter.
     """
-    return company_name.llm_company_name(product, howmany)
 
+    try:
+        _ = await authenticate(request)
 
-@app.post("/summarize")
-async def llm_summarize(file: UploadFile) -> str:
-    """
-    This is a docstring for llm_summarize.
-
-    Args:
-        file (UploadFile): UplaodFile
-
-    Returns:
-        str: Path to the audio file
-    """
-    filepath = await pdf_summarizer.pdf_reader(file)
-    res =  pdf_summarizer.pdf_summarizer(filepath)
-    return res
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+        res = company_name.llm_company_name(product, howmany)
+        return res
+    except HTTPException as e:
+        raise e 
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error") from e
