a9f3f042c01b037783fb9d19dc05a92bfa390d16
bignyap <bignyap@wolfram.com>
2024-01-18 19:23:35 +0530

    Create different class based on db schema

---
+++ src/chat_service/database/model.py
@@ -0,0 +1,24 @@
+"""
+Schema Models
+"""
+
+import datetime
+from pydantic import BaseModel
+
+class User(BaseModel):
+    '''Represents User class'''
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_active: bool
+
+class Thread(BaseModel):
+    '''Represents Thread class'''
+    name: str
+    date_created: datetime.time
+
+class Message(BaseModel):
+    '''Represents Message class'''
+    message: str
+    date_created: datetime.time
